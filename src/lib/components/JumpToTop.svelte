22b196063d1b042dfa6bcaf7289b513df28fa7d1
Károly Sárdi <skver@tuta.io>
2023-08-18 17:10:26 +0200

    add jump to top

---
+++ src/lib/components/JumpToTop.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+
+  let yPos: number = 0;
+
+  const jumpToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+</script>
+
+{#if yPos > 100}
+  <button
+    transition:fade={{ duration: 200 }}
+    class="z-10 fixed flex flex-col items-center justify-center bottom-10 right-10 h-14 w-14 rounded-full bg-neutral-800 border-neutral-700 border hover:bg-neutral-700 duration-200 drop-shadow-lg"
+    on:click={() => jumpToTop()}
+  >
+    <span class="material-symbols-outlined">arrow_upward</span>
+  </button>
+{/if}
+<slot />
+
+<svelte:window bind:scrollY={yPos} />
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
   import { page } from "$app/stores";
+  import JumpToTop from "$lib/components/JumpToTop.svelte";
   import RainEffect from "$lib/components/RainEffect.svelte";
   import SideMenu from "$lib/components/SideMenu.svelte";
   import Window from "$lib/components/Window.svelte";
@@ -12,11 +13,13 @@
   <link rel="icon" href="" />
 </svelte:head>
 
-<RainEffect />
-<SideMenu />
-{#key $page.url.pathname}
-  <Window class="w-full">
-    <slot />
-  </Window>
-{/key}
-<ModalRoot />
+<JumpToTop>
+  <RainEffect />
+  <SideMenu />
+  {#key $page.url.pathname}
+    <Window class="w-full">
+      <slot />
+    </Window>
+  {/key}
+  <ModalRoot />
+</JumpToTop>
