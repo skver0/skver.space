a8263fb9d2184d982e71b4060ec9e75714b1568d
Károly Sárdi <skver@tuta.io>
2023-08-21 00:37:38 +0200

    make button go cool

--- src/lib/components/JumpToTop.svelte
+++ src/lib/components/JumpToTop.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { fade } from "svelte/transition";
+  import { fade, fly } from "svelte/transition";
 
   let yPos: number = 0;
 
@@ -9,13 +9,19 @@
 </script>
 
 {#if yPos > 100}
-  <button
+  <div
     transition:fade={{ duration: 200 }}
     class="z-10 fixed flex flex-col items-center justify-center bottom-10 right-10 h-14 w-14 rounded-full dark:bg-neutral-800 bg-neutral-100 dark:border-neutral-700 border-neutral-400 border dark:hover:bg-neutral-700 hover:bg-neutral-300 duration-200 drop-shadow-lg"
-    on:click={() => jumpToTop()}
   >
-    <span class="material-symbols-outlined">arrow_upward</span>
-  </button>
+    <button
+      in:fly={{ y: 100, duration: 200 }}
+      out:fly={{ y: -100, duration: 200 }}
+      on:click={() => jumpToTop()}
+      class="flex items-center justify-center h-full w-full"
+    >
+      <span class="material-symbols-outlined">arrow_upward</span>
+    </button>
+  </div>
 {/if}
 <slot />
 
