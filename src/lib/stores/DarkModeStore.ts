20b7860834ff83862455e6a3c6ff943ab1cb6768
Károly Sárdi <skver@tuta.io>
2024-06-07 16:57:47 +0200

    fix darkmode

--- src/lib/stores/DarkModeStore.ts
+++ src/lib/stores/DarkModeStore.ts
@@ -1,7 +1,7 @@
 import { writable } from "svelte/store";
 import { browser } from "$app/environment";
 
-export const darkModeStore = writable((browser && localStorage.getItem("darkMode") === "true") || false);
+export const darkModeStore = writable((browser && localStorage.getItem("darkMode") === "true") || (browser && window.matchMedia('(prefers-color-scheme: dark)').matches) || false);
 
 darkModeStore.subscribe((value) => {
   if (browser) {
